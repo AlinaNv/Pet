de10febe81b8eddd78cf33295813cabf3b5db84c
AlinaNv <nightingalova@yandex.ru>
2023-05-28 00:13:39 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Задание_1.py
@@ -0,0 +1,9 @@
+#coding=windows-1251
+print("Введите вид питомца")
+type=(input(""))
+print("Введите имя питомца")
+name=(input(""))
+print("Введите возраст питомца")
+age=(input(""))
+print("Это", type, "по имени", name, ".", "Возраст:", age)
+
